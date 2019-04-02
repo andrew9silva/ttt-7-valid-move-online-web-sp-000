52eb046ba7887174f85574f2dcb8b107fe4c7816
Andrew Silva <andrewsilva9@gmail.com>
2019-04-02 01:31:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,10 @@
 # code your #valid_move? method here
 def valid_move?(board, position)
   integer = position.to_i -1
-  if position_taken(board[integer])
+  if position_taken()
     true
   end
-end 
+end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
