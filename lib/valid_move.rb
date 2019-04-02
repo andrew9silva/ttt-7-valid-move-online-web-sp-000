7b37c82b543b36e3d714874aed22b1a993e24c8c
Andrew Silva <andrewsilva9@gmail.com>
2019-04-02 01:34:30 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, position)
   integer = position.to_i -1
-  if position_taken(board, integer) && integer.between (0, 8)
+  if position_taken(board, integer) && integer.between (0,8)
     true
   end
 end
